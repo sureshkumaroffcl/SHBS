8cb6ca7d832d660efb03fd19363cb50fc8b566e2
Sureshkumar <168164496+sureshkumaroffcl@users.noreply.github.com>
2024-04-26 14:53:46 +0530

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace SeminarHallBookingSystem
+{
+    public class Connection
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["HallConn"].ConnectionString;
+            return con;
+        }
+
+    }
+}
